d468e1b30a6a68bd9c6aafa3b3d7fe4cef7a408d
Ben Skinner <benjamin.skinner96@gmail.com>
2016-08-29 21:49:55 +1000

    Merged Quick Data into wutils

--- src/wutils.js
+++ src/wutils.js
@@ -1,4 +1,157 @@
 var wutils = {
+	data: {
+		/* ======= Quick Data Object =======
+			 - Author: Ben Skinner
+			 - Params: 
+			 	- val (initial value of data)
+			 - Return: 
+			 	- Quick Data Object
+		*/
+		create: function(val) {
+			/* ======= Creates Inlet =======
+				 - Author: Ben Skinner
+				 - Desc: Adds event listener to @evt to change value of data
+				 - Private internal function
+				 - Params: 
+				 	- qd (Quick Data reference)
+				 	- dom (Direct HTML DOM reference)
+				 	- evt (String of event to listen for)
+				 - Return: 
+				 	- Quick Data Object
+			*/
+			var CreateInlet = function(qd, dom, evt) {
+				//Temporary data value
+				var t = qd;
+				//Add event listener to input of DOM
+				dom.addEventListener(evt, function(e) {
+
+					t.update(e.target.value);
+				});
+				//Push input reference into array of inputs
+				qd.inlets.push(dom);
+				//Update data value
+				qd.update(qd.value);
+			}
+			/* ======= Creates Outlet =======
+				 - Author: Ben Skinner
+				 - Desc: Adds event listener to @evt to change value of data
+				 - Private internal function
+				 - Params: 
+				 	- qd (Quick Data reference)
+				 	- dom (Direct HTML DOM reference)
+					- callback (function callback called on data change)
+				 - Return: 
+				 	- Quick Data Object
+			*/
+			var CreateOutlet = function(qd, dom, callback) {
+				//Build outlet variable with dom reference, and outlet callback
+				var out = {
+					dom:dom,
+					callback:callback
+				};
+				//Push output reference
+				qd.outlets.push(out);
+				//Updata data value
+				qd.update(qd.value);
+			}
+
+			return {
+				value: val,
+				inlets: [],
+				outlets: [],
+				/* ======= Update data =======
+					 - Author: Ben Skinner
+					 - Desc: Updates value and updates all callbacks for inlets and outlets
+					 - Params: 
+					 	- v (value to set to)
+					 - Return: 
+					 	- Quick Data Object
+				*/
+				update: function(v) {
+					//Update internal value
+					this.value = v;
+					
+					//Update all outlets
+					for(var i=0; i<this.outlets.length; i++) {
+						this.outlets[i].callback(this.value, this.outlets[i].dom);
+					}
+
+					//Update all inlets
+					for(var i=0; i<this.inlets.length; i++) {
+						this.inlets[i].value = this.value;
+					}
+				},
+				/* ======= Adds inlet =======
+					 - Author: Ben Skinner
+					 - Desc: adds inlet to array
+					 - Params: 
+					 	- doms_str (identification of dom)
+					 	- evt (optional event string)
+					 - Return: 
+					 	- Quick Data Object
+				*/
+				inlet: function(doms_str, evt) {
+					//Select necessary DOM elements
+					var doms = wutils.dom.get(doms_str);
+
+					//Default setting of evt
+					if(evt === undefined)
+						evt = "input";
+
+					//If single DOM element
+					if(doms.constructor !== Array) {
+						CreateInlet(this, doms, evt);
+					} else {
+						//Otherwise loop and add all DOM elements
+						for(var i = 0; i < doms.length; ++i) {
+							CreateInlet(this, doms[i], evt);
+						}
+					}
+					return this;
+				},
+				/* ======= Adds outlet =======
+					 - Author: Ben Skinner
+					 - Desc: adds outlet to array
+					 - Params: 
+					 	- doms_str (identification of dom)
+					 	- callback (function to call on value updated)
+					 - Return: 
+					 	- Quick Data Object
+				*/
+				outlet: function(doms_str, callback) {
+					//Select necessary DOM elements
+					var doms = wutils.dom.get(doms_str);
+
+					//If single DOM element
+					if(doms.constructor !== Array) {
+						CreateOutlet(this, doms, callback);
+					} else {
+						//Otherwise loop and add all DOM elements
+						for(var i = 0; i < doms.length; ++i) {
+							CreateOutlet(this, doms[i], callback);
+						}
+					}
+					return this;
+				}
+			}
+		},
+		/* ======= Quick Data Presets =======
+			 - Author: Ben Skinner
+			 - Members: 
+			 	- HTML (Replaces innerHTML with value)
+			 	- Attribute (Sets attribute @attr with value)
+		*/
+		preset: {
+			HTML: function(value, dom) {
+				dom.innerHTML = value;
+			},
+			Attribute: function(attr) {
+				return function(value, dom) {
+					dom.setAttribute(attr, value);
+				}
+			}
+		}
+	},
 	dom: {
 		/* ======= Get element by ambiguous identifier =======
 			 - Author: Ben Skinner
