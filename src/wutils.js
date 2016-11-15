fd5f9870630dc13fd23530cac608115e3da713ed
Ben Skinner <benjamin.skinne@ad.aie.edu>
2016-11-16 10:14:26 +1100

    Added url query functionality

--- src/wutils.js
+++ src/wutils.js
@@ -422,6 +422,36 @@ var wutils = {
 			})
 		}
 	},
+	query: {
+		/* ======== Query URL ========
+			 - Author: Ben Skinner
+			 - Params: 
+			 	- key 		(post url string)
+			 - Detail: 
+				- Returns associated array of found paramaters
+		*/
+		url: function(qs) {
+			//If no paramater get current documents paramaters
+			if(!qs)
+				qs = document.location.search;
+
+			//Split string
+          	qs = qs.split('+').join(' ');
+
+          	//Create variables
+          	var params = {},
+            	tokens,
+              	re = /[?&]?([^=]+)=([^&]*)/g;
+
+            //Parse all tokens and execute
+          	while (tokens = re.exec(qs)) {
+            	params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+          	}
+
+          	//Return associated array
+          	return params;
+      }
+	},
 	string: {
 		/* ======= Combine strings with options =======
 			 - Author: Ben Skinner
