63817b4dea7475b90e10a61cec0342e90e2d2324
Ben Skinner <benjamin.skinner96@gmail.com>
2017-05-11 21:36:33 +1000

    Fixed storage problem, working correctly now

--- src/modules/wutils_data.js
+++ src/modules/wutils_data.js
@@ -5,9 +5,9 @@ wutils.data = {};
 
 /* ======= Quick Data Object =======
 	 - Author: Ben Skinner
-	 - Params: 
+	 - Params:
 	 	- val (initial value of data)
-	 - Return: 
+	 - Return:
 	 	- Quick Data Object
 */
 wutils.data.create = function(val) {
@@ -15,11 +15,11 @@ wutils.data.create = function(val) {
 		 - Author: Ben Skinner
 		 - Desc: Adds event listener to @evt to change value of data
 		 - Private internal function
-		 - Params: 
+		 - Params:
 		 	- qd (Quick Data reference)
 		 	- dom (Direct HTML DOM reference)
 		 	- evt (String of event to listen for)
-		 - Return: 
+		 - Return:
 		 	- Quick Data Object
 	*/
 	var CreateInlet = function(qd, dom, evt) {
@@ -27,11 +27,20 @@ wutils.data.create = function(val) {
 		var t = qd;
 		//Add event listener to input of DOM
 		dom.addEventListener(evt, function(e) {
+			var val = (e.target.isContentEditable) ? e.target.innerHTML : e.target.value;
 
-			t.update(e.target.value);
+			t.update(val);
+			//t.update(e.target.value);
+			console.log(e.target.isContentEditable);
 		});
 		//Push input reference into array of inputs
 		qd.inlets.push(dom);
+
+		if(dom.isContentEditable) {
+			console.log("making inlet's outlet")
+			CreateOutlet(qd, wutils.data.presets.HTML, dom);
+		}
+
 		//Update data value
 		qd.update(qd.value);
 	}
@@ -39,11 +48,11 @@ wutils.data.create = function(val) {
 		 - Author: Ben Skinner
 		 - Desc: Adds event listener to @evt to change value of data
 		 - Private internal function
-		 - Params: 
+		 - Params:
 		 	- qd (Quick Data reference)
-		 	- dom (Direct HTML DOM reference)
 			- callback (function callback called on data change)
-		 - Return: 
+			- data_ref ()
+		 - Return:
 		 	- Quick Data Object
 	*/
 	var CreateOutlet = function(qd, callback, data_ref) {
@@ -60,39 +69,43 @@ wutils.data.create = function(val) {
 
 	return {
 		value: val,
+		setValue: function(v) {
+			this.value = v;
+		},
+		getValue: function() { return this.value; },
 		inlets: [],
 		outlets: [],
 		/* ======= Update data =======
 			 - Author: Ben Skinner
 			 - Desc: Updates value and updates all callbacks for inlets and outlets
-			 - Params: 
+			 - Params:
 			 	- v (value to set to)
-			 - Return: 
+			 - Return:
 			 	- Quick Data Object
 		*/
 		update: function(v) {
 			//Update internal value if argument
 			if(v !== undefined)
-				this.value = v;
-			
+				this.setValue(v);
+
 			//Update all outlets
 			for(var i=0; i<this.outlets.length; i++) {
 				//Pass through value and data reference
-				this.outlets[i].callback(this.value, this.outlets[i].data);
+				this.outlets[i].callback(this.getValue(), this.outlets[i].data);
 			}
 
 			//Update all inlets
 			for(var i=0; i<this.inlets.length; i++) {
-				this.inlets[i].value = this.value;
+				this.inlets[i].value = this.getValue();
 			}
 		},
 		/* ======= Adds inlet =======
 			 - Author: Ben Skinner
 			 - Desc: adds inlet to array
-			 - Params: 
+			 - Params:
 			 	- doms_str (identification of dom)
 			 	- evt (optional event string)
-			 - Return: 
+			 - Return:
 			 	- Quick Data Object
 		*/
 		inlet: function(doms_str, evt) {
@@ -117,10 +130,10 @@ wutils.data.create = function(val) {
 		/* ======= Adds outlet =======
 			 - Author: Ben Skinner
 			 - Desc: adds outlet to array
-			 - Params: 
+			 - Params:
 				- callback (function to call on value updated)
 			 	- data_ref (external data reference to be passed through)
-			 - Return: 
+			 - Return:
 			 	- Quick Data Object
 		*/
 		outlet: function(callback, data_ref) {
@@ -133,7 +146,7 @@ wutils.data.create = function(val) {
 
 /* ======= Quick Data Presets =======
 	 - Author: Ben Skinner
-	 - Members: 
+	 - Members:
 	 	- HTML (Replaces innerHTML with value)
 	 	- Attribute (Sets attribute @attr with value)
 */
@@ -176,6 +189,6 @@ wutils.data.presets = {
 		}
 	},
 	LocalStorage: function(value, data_ref) {
-		
+
 	}
 }
