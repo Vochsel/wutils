01dcc6aedd7470b7a1da7997b37fc9442329dff7
Ben Skinner <benjamin.skinner96@gmail.com>
2018-04-02 20:00:33 +1000

    Improved npm compatability

---
+++ index.js
@@ -0,0 +1,18 @@
+/*!
+ * wutils: Web Utils
+ * Copyright(c) 2018 Ben Skinner (@Vochsel)
+ */
+
+"use strict";
+
+var wutils = require("./src/wutils.js");
+
+wutils.conversion = require("./src/modules/wutils_conversion.js");
+wutils.data = require("./src/modules/wutils_data.js");
+wutils.dom = require("./src/modules/wutils_dom.js");
+wutils.file = require("./src/modules/wutils_file.js");
+wutils.input = require("./src/modules/wutils_input.js");
+wutils.query = require("./src/modules/wutils_query.js");
+wutils.string = require("./src/modules/wutils_string.js");
+
+module.exports = wutils;
